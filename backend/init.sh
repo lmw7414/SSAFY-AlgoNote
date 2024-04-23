164b15b83d19b12a8ddcbd4b85a074cdd30be542
김희수 <wlskaka4@gmail.com>
2024-04-23 17:48:43 +0900

    #3 - feat [infra] dockerfile, init.sh 추가

---
+++ backend/init.sh
@@ -0,0 +1,5 @@
+sudo docker rm spring-container --force
+
+sudo docker build --no-cache -t spring-image .
+
+sudo docker run -p 8080:8081 -d -v /home/ubuntu/images:/images --name spring-container spring-image
