92bf8f3eec45f3b5eafccecff6d5fb28ec145f7a
김희수 <wlskaka4@gmail.com>
2024-04-23 15:55:34 +0900

    #1 - feat: [BE] first commit

---
+++ backend/src/main/java/com/ssafy/algonote/AlgonoteApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.algonote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgonoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgonoteApplication.class, args);
+	}
+
+}
