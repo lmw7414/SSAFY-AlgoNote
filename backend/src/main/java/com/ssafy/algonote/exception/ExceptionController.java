29f07d539eac9cb1f339b5c14fe6ba21ab355729
김성재 <sngjae972@gmail.com>
2024-04-24 13:04:39 +0900

    #7 - chore: [BE] DTO 타입 변환(class -> record)

--- backend/src/main/java/com/ssafy/algonote/exception/ExceptionController.java
+++ backend/src/main/java/com/ssafy/algonote/exception/ExceptionController.java
@@ -1,8 +1,5 @@
 package com.ssafy.algonote.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,19 +12,15 @@ public class ExceptionController {
     @ExceptionHandler({
         CustomException.class
     })
-    public ResponseEntity<ExceptionResponse> customRequestException
-        (final CustomException e) {
+    public ResponseEntity<ExceptionResponse> handlerCustomException(final CustomException e) {
         log.warn("api Exception : {}", e.getErrorCode());
         return ResponseEntity.status(e.getErrorCode().getHttpStatus())
             .body(new ExceptionResponse(e.getErrorCode(), e.getMessage()));
     }
 
-    @Getter
-    @ToString
-    @AllArgsConstructor
-    public static class ExceptionResponse {
-        private ErrorCode errorCode;
-        private String message;
-    }
+    public record ExceptionResponse(
+        ErrorCode errorCode,
+        String message
+    ) {}
 
 }
