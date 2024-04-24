850b41684e2cc7fe266adb1e1c996b6a9cd58303
김희수 <wlskaka4@gmail.com>
2024-04-24 10:16:11 +0900

    #5 - feat : [infra] 백엔드 CI/CD 자동화 테스트2

--- backend/src/main/java/com/ssafy/algonote/AlgonoteApplication.java
+++ backend/src/main/java/com/ssafy/algonote/AlgonoteApplication.java
@@ -8,11 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlgonoteApplication {
 
 	public static void main(String[] args) {
-		// Dotenv dotenv = Dotenv.load();
-		// System.setProperty("spring.datasource.url", dotenv.get("LOCAL_MYSQL_URL"));
-		// System.setProperty("spring.datasource.username", dotenv.get("LOCAL_MYSQL_USERNAME"));
-		// System.setProperty("spring.datasource.password", dotenv.get("LOCAL_MYSQL_PASSWORD"));
-
 		SpringApplication.run(AlgonoteApplication.class, args);
 	}
 
