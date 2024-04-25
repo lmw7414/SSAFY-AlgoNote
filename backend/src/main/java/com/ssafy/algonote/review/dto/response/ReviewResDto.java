865317703241dd49f5657949d1a1d11fa5d46d9c
김성재 <sngjae972@gmail.com>
2024-04-25 14:16:00 +0900

    #13 - refactor: [BE] ReviewResDto 에 timestamp 컬럼 추가

--- backend/src/main/java/com/ssafy/algonote/review/dto/response/ReviewResDto.java
+++ backend/src/main/java/com/ssafy/algonote/review/dto/response/ReviewResDto.java
@@ -2,6 +2,7 @@ package com.ssafy.algonote.review.dto.response;
 
 import com.ssafy.algonote.member.dto.response.MemberResDto;
 import com.ssafy.algonote.review.domain.Review;
+import java.time.LocalDateTime;
 import lombok.Builder;
 
 @Builder
@@ -9,7 +10,9 @@ public record ReviewResDto(
     MemberResDto member,
     int startLine,
     int endLine,
-    String content
+    String content,
+    LocalDateTime createdAt,
+    LocalDateTime modifiedAt
 ) {
     public static ReviewResDto from(Review review) {
         return ReviewResDto.builder()
@@ -17,6 +20,8 @@ public record ReviewResDto(
             .startLine(review.getStartLine())
             .endLine(review.getEndLine())
             .content(review.getContent())
+            .createdAt(review.getCreatedAt())
+            .modifiedAt(review.getModifiedAt())
             .build();
     }
 }
