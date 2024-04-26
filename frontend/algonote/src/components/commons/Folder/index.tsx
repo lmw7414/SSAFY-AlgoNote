f62395567986e06bffa6f2a54330cd5c2ae72f9e
이은규 <eung9702@gmail.com>
2024-04-26 17:55:11 +0900

    feat: [FE] 폴더 컴포넌트 개발

---
+++ frontend/algonote/src/components/commons/Folder/index.tsx
@@ -0,0 +1,60 @@
+import { Bronze, Silver, Gold, Platinum, Diamond, Ruby } from '../Tiers'
+import s from './Folder.module.scss'
+import TextCarousel from './TextCarousel'
+
+interface FolderProps {
+  tier: string
+  problemId: number
+  problemTitle: string
+  notes: Array<string>
+}
+const Folder = ({ tier, problemId, problemTitle, notes }: FolderProps) => {
+  return (
+    <div className={s.wrapper}>
+      <div className={s.tierContainer}>
+        {tier[0] === 'B' ? (
+          <>
+            <p className={s.tier}>{tier}</p>
+            <Bronze />
+          </>
+        ) : tier[0] === 'S' ? (
+          <>
+            <p className={s.tier}>{tier}</p>
+            <Silver />
+          </>
+        ) : tier[0] === 'G' ? (
+          <>
+            <p className={s.tierBlack}>{tier}</p>
+            <Gold />
+          </>
+        ) : tier[0] === 'P' ? (
+          <>
+            <p className={s.tierBlack}>{tier}</p>
+            <Platinum />
+          </>
+        ) : tier[0] === 'D' ? (
+          <>
+            <p className={s.tier}>{tier}</p>
+            <Diamond />
+          </>
+        ) : tier[0] === 'R' ? (
+          <>
+            <p className={s.tier}>{tier}</p>
+            <Ruby />
+          </>
+        ) : null}
+      </div>
+      <div className={s.container}>
+        <div className={s.top}>
+          <p className={s.problemId}>백준 {problemId}</p>
+          <p className={s.problemTitle}>{problemTitle}</p>
+        </div>
+        <div className={s.carouselContainer}>
+          <TextCarousel notes={notes} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Folder
