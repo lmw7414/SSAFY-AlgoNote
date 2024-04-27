b542e5dcd33c225ddcbcb3ccd3b1fed274aaebc9
이민우 <lmw7414@naver.com>
2024-04-27 11:24:54 +0900

    #26 - chore: [BE] `problem`api path 수정
    problem -> problems로 변경

--- backend/src/main/java/com/ssafy/algonote/config/security/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/algonote/config/security/SecurityConfig.java
@@ -24,7 +24,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private static final String[] AUTH_WHITELIST = {
-        "/auth/**", "/member/**", "/problem/**"
+        "/auth/**", "/member/**", "/problems/**"
     };
 
     private final JwtUtil jwtUtil;
--- backend/src/main/java/com/ssafy/algonote/problem/controller/ProblemController.java
+++ backend/src/main/java/com/ssafy/algonote/problem/controller/ProblemController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/problem")
+@RequestMapping("/problems")
 @RequiredArgsConstructor
 public class ProblemController {
 
