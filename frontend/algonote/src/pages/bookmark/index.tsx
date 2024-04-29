a87f1f76f2aa4b659b5c5a18197bf2a2ef34d84c
조은영 <hdtv0218@gmail.com>
2024-04-29 17:52:47 +0900

    #30 - style : gap 수정

--- frontend/algonote/src/pages/bookmark/index.tsx
+++ frontend/algonote/src/pages/bookmark/index.tsx
@@ -1,10 +1,52 @@
+import Styles from './bookmark.module.scss'
 import Note from '@/components/commons/Bookmark/Note'
+import { FilterButton } from '@/components/commons/Buttons/Button'
 import SearchInput from '@/components/commons/SearchInput'
+
+const tier = ['브론즈', '실버', '골드', '플레티넘', '다이아', '루비']
+const category = ['구현', '그리디', '그래프', '플래티넘', 'DP', '자료구조']
 
 const Bookmark = () => {
   return (
     <div>
       <SearchInput />
+      <div className={Styles.bookmark}>
+        <div>티어</div>
+        <div className={Styles.filterButton}>
+          {tier.map((it, index) => {
+            const tierKey = `tier-${index}`
+            return (
+              <div key={tierKey}>
+                <FilterButton
+                  text={it}
+                  onClick={() => console.log('눌렸음')}
+                  className="search"
+                  initialClicked={false}
+                />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+
+      <div className={Styles.bookmark}>
+        <div>유형</div>
+        <div className={Styles.filterButton}>
+          {category.map((it, index) => {
+            const categoryKey = `tier-${index}`
+            return (
+              <div key={categoryKey}>
+                <FilterButton
+                  text={it}
+                  onClick={() => console.log('눌렸음')}
+                  className="search"
+                  initialClicked={false}
+                />
+              </div>
+            )
+          })}
+        </div>
+      </div>
       <Note />
     </div>
   )
