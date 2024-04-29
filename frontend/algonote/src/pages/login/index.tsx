f34b0ff35f8ba882a41c0056d34bf0a7fb26c7f6
이은규 <eung9702@gmail.com>
2024-04-29 14:45:24 +0900

    feat: [FE] 로그인 페이지 개발

--- frontend/algonote/src/pages/login/index.tsx
+++ frontend/algonote/src/pages/login/index.tsx
@@ -1,5 +1,52 @@
+import { useState, ChangeEvent } from 'react'
+import { Button, Input } from '@nextui-org/react'
+import Image from 'next/image'
+import s from './login.module.scss'
+
 const Login = () => {
-  return <h1>로그인</h1>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleEmail = (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value
+
+    setEmail(newValue)
+  }
+
+  const handlePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value
+    setPassword(newValue)
+  }
+
+  return (
+    <div className={s.container}>
+      <div className={s.loginContainer}>
+        <Image
+          src="/images/loginLogo.png"
+          alt="loginLogo"
+          width={320}
+          height={145}
+        />
+        <Input
+          type="email"
+          label="이메일"
+          variant="underlined"
+          value={email}
+          onChange={handleEmail}
+        />
+        <Input
+          type="password"
+          label="비밀번호"
+          variant="underlined"
+          value={password}
+          onChange={handlePassword}
+        />
+        <Button className={s.btn}>로그인</Button>
+        <p>아직 회원이 아니신가요?</p>
+        <a href="./signup">회원가입하기</a>
+      </div>
+    </div>
+  )
 }
 
 export default Login
