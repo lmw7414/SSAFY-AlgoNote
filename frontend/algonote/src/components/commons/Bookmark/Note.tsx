85f65b43ddd0727a53fdadd45d00f1a669dde40d
조은영 <hdtv0218@gmail.com>
2024-05-03 15:57:42 +0900

    #46 - feat : [FE] 북마크 페이지 좋아요 삭제

--- frontend/algonote/src/components/commons/Bookmark/Note.tsx
+++ frontend/algonote/src/components/commons/Bookmark/Note.tsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react'
-import HeartOffSVG from '@public/images/heart.svg'
-import HeartSVG from '@public/images/redHeart.svg'
+// import HeartOffSVG from '@public/images/heart.svg'
+// import HeartSVG from '@public/images/redHeart.svg'
 import Tier from '@public/images/tier.svg'
 import Image from 'next/image'
 import styles from './Note.module.scss'
 import bookmarkListApi from '@/apis/bookmarkAxios'
-import likeAxios from '@/apis/likeAxios'
-import ImageToggle from '@/components/commons/Buttons/ImageToggle'
+// import likeAxios from '@/apis/likeAxios'
+// import ImageToggle from '@/components/commons/Buttons/ImageToggle'
 
 interface Note {
   id: number
@@ -33,14 +33,14 @@ interface Bookmark {
 
 const Notes = () => {
   const [bookmarks, setBookmarks] = useState<Bookmark[]>([])
-  const [heartIsOff, setHeartIsOff] = useState<boolean[]>([])
+  // const [heartIsOff, setHeartIsOff] = useState<boolean[]>([])
 
   useEffect(() => {
     const fetchBookmarks = async () => {
       try {
         const response = await bookmarkListApi()
         setBookmarks(response.data)
-        setHeartIsOff(response.data.map(() => true)) // 모든 하트를 꺼진 상태로 초기화
+        // setHeartIsOff(response.data.map(() => true)) // 모든 하트를 꺼진 상태로 초기화
       } catch (err) {
         console.log(err)
       }
@@ -49,65 +49,64 @@ const Notes = () => {
     fetchBookmarks()
   }, [])
 
-  const handleHeartState = async (index: number, id: number) => {
-    const response = await likeAxios(id)
+  // const handleHeartState = async (index: number, id: number) => {
+  //   const response = await likeAxios(id)
 
-    const newHeartState = [...heartIsOff]
-    newHeartState[index] = response.data.hearted
-    setHeartIsOff(newHeartState)
+  //   const newHeartState = [...heartIsOff]
+  //   newHeartState[index] = response.data.hearted
+  //   setHeartIsOff(newHeartState)
 
-    // hearCnt 증가
+  //   // hearCnt 증가
 
-    const updatedBookmarks = bookmarks.map(async (bookmark, idx) => {
-      if (index === idx) {
-        const newHeartCount = newHeartState[index]
-          ? bookmark.note.heartCnt + 1
-          : bookmark.note.heartCnt - 1
-        return {
-          ...bookmark,
-          note: { ...bookmark.note, heartCnt: newHeartCount },
-        }
-      }
-      return bookmark
-    })
-    const resolvedBookmarks = await Promise.all(updatedBookmarks)
-    setBookmarks(resolvedBookmarks)
-  }
+  //   const updatedBookmarks = bookmarks.map(async (bookmark, idx) => {
+  //     if (index === idx) {
+  //       const newHeartCount = newHeartState[index]
+  //         ? bookmark.note.heartCnt + 1
+  //         : bookmark.note.heartCnt - 1
+  //       return {
+  //         ...bookmark,
+  //         note: { ...bookmark.note, heartCnt: newHeartCount },
+  //       }
+  //     }
+  //     return bookmark
+  //   })
+  //   const resolvedBookmarks = await Promise.all(updatedBookmarks)
+  //   setBookmarks(resolvedBookmarks)
+  // }
 
   return (
     <div className={styles.frame}>
-      {heartIsOff &&
-        bookmarks.map((it, index: number) => {
-          const key = `${it.problem.title}-${index}`
+      {bookmarks.map((it, index: number) => {
+        const key = `${it.problem.title}-${index}`
 
-          return (
-            <div key={key} className={styles.note}>
-              <div className={styles.content}>
-                <div className={styles.problem}>
-                  <div className={styles.tierImage}>
-                    <Image src={Tier} alt="티어" />
-                  </div>
-                  <div className={styles.problemTitle}>{it.problem.title}</div>
+        return (
+          <div key={key} className={styles.note}>
+            <div className={styles.content}>
+              <div className={styles.problem}>
+                <div className={styles.tierImage}>
+                  <Image src={Tier} alt="티어" />
                 </div>
-                <div className={styles.note_title}>{it.note.title}</div>
-                <div className={styles.details}>
-                  <ImageToggle
-                    isOff={heartIsOff[index]}
-                    onClick={() => handleHeartState(index, it.note.id)}
-                    offImg={HeartOffSVG}
-                    onImg={HeartSVG}
-                    width="1.6rem"
-                    height="1.6rem"
-                  />
-                  <div className={styles.countNickname}>
-                    <div>{it.note.heartCnt}</div>
-                    <div className={styles.nickname}>{it.member.nickname}</div>
-                  </div>
+                <div className={styles.problemTitle}>{it.problem.title}</div>
+              </div>
+              <div className={styles.note_title}>{it.note.title}</div>
+              <div className={styles.details}>
+                {/* <ImageToggle
+                  isOff={heartIsOff[index]}
+                  onClick={() => handleHeartState(index, it.note.id)}
+                  offImg={HeartOffSVG}
+                  onImg={HeartSVG}
+                  width="1.6rem"
+                  height="1.6rem"
+                /> */}
+                <div className={styles.countNickname}>
+                  {/* <div>{it.note.heartCnt}</div> */}
+                  <div className={styles.nickname}>{it.member.nickname}</div>
                 </div>
               </div>
             </div>
-          )
-        })}
+          </div>
+        )
+      })}
     </div>
   )
 }
