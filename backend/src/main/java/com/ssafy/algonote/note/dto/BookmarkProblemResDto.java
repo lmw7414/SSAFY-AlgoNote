3e4de50426017c19cf1bfea36dd1d893444578ee
김성재 <sngjae972@gmail.com>
2024-05-04 16:24:01 +0900

    #52 - refactor : [BE] 북마크 목록 조회 시 각 문제에 해당하는 태그 정보 추가

--- backend/src/main/java/com/ssafy/algonote/note/dto/BookmarkProblemResDto.java
+++ backend/src/main/java/com/ssafy/algonote/note/dto/BookmarkProblemResDto.java
@@ -1,19 +1,24 @@
 package com.ssafy.algonote.note.dto;
 
 import com.ssafy.algonote.problem.domain.Problem;
+import com.ssafy.algonote.problem.domain.Tag;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.Builder;
 
 @Builder
 public record BookmarkProblemResDto(
         Long id,
         String title,
-        int tier
+        int tier,
+        Set<String> tags
 ) {
     public static BookmarkProblemResDto from(Problem problem) {
         return BookmarkProblemResDto.builder()
                 .id(problem.getId())
                 .title(problem.getTitle())
                 .tier(problem.getTier())
+                .tags(problem.getTags().stream().map(Tag::getNameEn).collect(Collectors.toUnmodifiableSet()))
                 .build();
     }
 }
