068702bc76df60987d917962956a1bba3ac254b4
euny0ung <hdtv0218@gmail.com>
2024-05-04 17:49:39 +0900

    #46 - feat : 레벨별 이미지 변경 추가

--- frontend/algonote/src/components/commons/Tier/index.tsx
+++ frontend/algonote/src/components/commons/Tier/index.tsx
@@ -1,4 +1,49 @@
 // it.problem.tier
-const tierArr = ['Unrated', 'Bronze']
+import Image from 'next/image'
 
-const TierImg = (tier) => {}
+interface TierImgProps {
+  tier: number
+}
+
+const TierImg = ({ tier }: TierImgProps) => {
+  let folder
+  let file
+
+  switch (true) {
+    case tier <= 5:
+      folder = 'bronze'
+      file = 6 - tier
+      break
+    case tier <= 10:
+      folder = 'silver'
+      file = 11 - tier
+      break
+    case tier <= 15:
+      folder = 'gold'
+      file = 16 - tier
+      break
+    case tier <= 20:
+      folder = 'platinum'
+      file = 21 - tier
+      break
+    case tier <= 25:
+      folder = 'dia'
+      file = 26 - tier
+      break
+    default:
+      folder = 'unrated'
+  }
+
+  return (
+    <div>
+      <Image
+        src={`/images/tier/${folder}/${folder}${file}.png`}
+        alt={`문제레벨${folder}${file}`}
+        width={20}
+        height={15}
+      />
+    </div>
+  )
+}
+
+export default TierImg
