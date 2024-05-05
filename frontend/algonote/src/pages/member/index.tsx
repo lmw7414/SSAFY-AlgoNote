46610a85aa8ce74e1278092791849220020854a5
조은영 <hdtv0218@gmail.com>
2024-05-05 17:11:21 +0900

    #63 - feat : 멤버 아이디 조회 방식 변경

--- frontend/algonote/src/pages/member/index.tsx
+++ frontend/algonote/src/pages/member/index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import style from './member.module.scss'
 import myInfo from '@/apis/userInfoAxios'
-import useUserInfo from '@/stores/user-store'
 
 interface UserInfo {
   memberId: number
@@ -12,22 +11,19 @@ interface UserInfo {
 }
 
 const User = () => {
-  const { userInfo } = useUserInfo()
   // useState를 null 가능한 UserInfo 타입으로 설정합니다. 초기 상태를 null로 설정합니다.
   const [userDetails, setUserDetails] = useState<UserInfo | null>(null)
 
   useEffect(() => {
     const fetchMyInfo = async () => {
-      if (userInfo.memberId) {
-        try {
-          const response = await myInfo(userInfo.memberId)
-          // API 응답을 상태에 저장하기 전에 형식이 맞는지 확인합니다.
-          if (response && typeof response === 'object') {
-            setUserDetails(response.data)
-          }
-        } catch (error) {
-          console.error('내 정보 가져오기 실패:', error)
+      try {
+        const response = await myInfo()
+        // API 응답을 상태에 저장하기 전에 형식이 맞는지 확인합니다.
+        if (response && typeof response === 'object') {
+          setUserDetails(response.data)
         }
+      } catch (error) {
+        console.error('내 정보 가져오기 실패:', error)
       }
     }
 
