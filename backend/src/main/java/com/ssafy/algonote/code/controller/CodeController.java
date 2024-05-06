99ff9c8085f4ef8d9e401cba062d270779bea7c3
김성재 <sngjae972@gmail.com>
2024-05-06 17:26:10 +0900

    docs : [BE] docs 정보 추가

--- backend/src/main/java/com/ssafy/algonote/code/controller/CodeController.java
+++ backend/src/main/java/com/ssafy/algonote/code/controller/CodeController.java
@@ -5,6 +5,8 @@ import com.ssafy.algonote.code.dto.request.ComplexityReqDto;
 import com.ssafy.algonote.code.dto.response.AnalyzeResDto;
 import com.ssafy.algonote.code.dto.response.ComplexityResDto;
 import com.ssafy.algonote.code.service.CodeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Code API", description = "코드 분석 관련 API")
 @RestController
 @RequestMapping("/codes")
 @RequiredArgsConstructor
@@ -24,6 +27,10 @@ public class CodeController {
         return ResponseEntity.ok(codeService.analyzeCode(dto));
     }
 
+    @Operation(
+        summary = "복잡도 계산",
+        description = "코드에 대한 시간 복잡도와 공간 복잡도를 계산합니다."
+    )
     @PostMapping("/complexity")
     public ResponseEntity<ComplexityResDto> getComplexity(@RequestBody ComplexityReqDto dto) {
         return ResponseEntity.ok(codeService.getComplexity(dto));
