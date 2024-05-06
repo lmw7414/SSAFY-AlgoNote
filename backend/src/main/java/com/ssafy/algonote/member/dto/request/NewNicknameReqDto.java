af164a8822e1734df0b5593e92c99b7dfecbdfc0
김성재 <sngjae972@gmail.com>
2024-05-06 14:31:29 +0900

    #65 - fix : [BE] record를 클래스로 변환..

--- backend/src/main/java/com/ssafy/algonote/member/dto/request/NewNicknameReqDto.java
+++ backend/src/main/java/com/ssafy/algonote/member/dto/request/NewNicknameReqDto.java
@@ -1,8 +1,8 @@
 package com.ssafy.algonote.member.dto.request;
 
-public record NewNicknameReqDto(
-        String nickname
-) {
+public class NewNicknameReqDto{
+    String nickname;
+
     public NewNicknameReqDto(String nickname) {
         this.nickname = nickname.trim();
     }
