49676dca347fe692ca158ae49c7f49d99a4f0131
이민우 <lmw7414@naver.com>
2024-05-06 16:52:08 +0900

    #48 - chore: [BE] CodeController 엔드포인트 복수형으로 변경

--- backend/src/main/java/com/ssafy/algonote/code/controller/CodeController.java
+++ backend/src/main/java/com/ssafy/algonote/code/controller/CodeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/code")
+@RequestMapping("/codes")
 @RequiredArgsConstructor
 public class CodeController {
 
