bfc6d28c3486f9d1e9cd1a6d7172971f53866c0c
김성현 <oilater@naver.com>
2024-05-08 16:37:45 +0900

    feat: [FE] 노트 API 연결 및 세부 UI 수정

--- frontend/algonote/src/apis/regist-noteAxios.tsx
+++ frontend/algonote/src/apis/regist-noteAxios.tsx
@@ -1,11 +1,42 @@
+import { getCookie } from '@/utils/cookie'
 import { axiosAuthApi } from '@/utils/instance'
 
-const registNote = async (problemId: number, title: string, number: string) => {
-  return axiosAuthApi().post(`/api/notes`, {
-    problemId,
-    title,
-    number,
-  })
+// 노트 등록 (현재 임시 문제 번호값 넣어놓은 상태 )
+const registNote = async (
+  problemId: number,
+  title: string,
+  content: string,
+) => {
+  try {
+    await axiosAuthApi()
+      .post(`/api/notes`, {
+        problemId,
+        title,
+        content,
+      })
+      .then((res) => {
+        console.log(res)
+      })
+  } catch (e) {
+    console.log(e)
+  }
 }
 
-export default registNote
+// 제출 번호로 제출 이력 조회 (로그인 후 노트 작성 페이지 진입 시 호출, 현재 임시 번호 넣어놓음)
+const getSubmissionListByMemberIdAndProblemId = async (problemId: number) => {
+  try {
+    const memberId = getCookie('memberId')
+
+    return await axiosAuthApi()
+      .get(`/api/submissions?memberId=${memberId}&problemId=${problemId}`)
+      .then((res) => {
+        console.log(res.data)
+        return res.data
+      })
+  } catch (e) {
+    console.log(e)
+    return []
+  }
+}
+
+export { registNote, getSubmissionListByMemberIdAndProblemId }
--- frontend/algonote/src/pages/writenote/index.tsx
+++ frontend/algonote/src/pages/writenote/index.tsx
@@ -1,30 +1,67 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import s from './writenote.module.scss'
+import {
+  getSubmissionListByMemberIdAndProblemId,
+  registNote,
+} from '@/apis/regist-noteAxios'
+import { SimpleButton } from '@/components/commons/Buttons/Button'
 import SubmitList from '@/components/commons/SubmitList'
 import SubmitListTitle from '@/components/commons/SubmitListTitle'
 import Tabs from '@/components/commons/Tabs'
-import { SimpleButton } from '@/components/commons/Buttons/Button'
+import useNoteStore from '@/stores/note-store'
+
+interface SubmissionHistory {
+  code: string
+  language: string
+  length: number
+  memorySize: number
+  result: string
+  runningTime: number
+  submissionId: number
+  submissionTime: string
+}
 
 const WriteNote = () => {
+  const currentDate = new Date()
+
+  const [submissionList, setSubmissionList] = useState<SubmissionHistory[]>([]) // 제출 이력
+  useEffect(() => {
+    const fetchData = async () => {
+      const list = await getSubmissionListByMemberIdAndProblemId(1012)
+      setSubmissionList(() => list)
+    }
+    fetchData()
+  }, [])
+
   const [isCollapsed, setIsCollapsed] = useState(false)
+  const { tabs, curSelectedIdx } = useNoteStore()
   const buttonClickHandler = () => {
     setIsCollapsed(!isCollapsed)
-    console.log('버튼을 클릭했습니다.')
   }
 
   const handleClickButton = () => {
-    console.log('버튼 클릭')
+    const { title } = tabs[curSelectedIdx]
+    const { content } = tabs[curSelectedIdx]
+    registNote(1012, title, content)
   }
 
-  const rightStyle = isCollapsed ? { flex: 21 } : { flex: 3 }
-  const buttonSecStyle = isCollapsed ? { width: '47.5%' } : { width: '37.5%' }
+  // UI 관련 스타일
+  let resultColor = ''
+  const [correctColor, incorrectColor] = ['#3c87fe', '#fb4444']
+  const rightStyle = isCollapsed ? { flex: 30 } : { flex: 3 }
+
+  const leftStyle = isCollapsed
+    ? { justifyContent: 'center', paddingLeft: 0, paddingRight: 0 }
+    : { justifyContent: 'space-between', paddingLeft: '1rem' }
+
+  const buttonSecStyle = isCollapsed ? { width: '48.5%' } : { width: '37.8%' }
   return (
     <div className={s.wrapper}>
       <div className={s.left}>
         <div className={s.submitWrapper}>
-          <div className={s.submitTitleBox}>
+          <div className={s.submitTitleBox} style={leftStyle}>
             {!isCollapsed && <span>제출 이력</span>}
             <button
               className={s.button}
@@ -40,27 +77,55 @@ const WriteNote = () => {
               />
             </button>
           </div>
-          {/* isCollapsed가 false일 때만 내용을 보여줍니다. */}
+          {/* 화면 축소 버튼 누르지 않았을 때 제출 이력을 보여줌 */}
           {!isCollapsed && (
             <div className={s.submitListBox}>
               <div className={s.submitListTitle}>
                 <SubmitListTitle />
               </div>
               <div className={s.submitList}>
-                <SubmitList
-                  number="1"
-                  result="맞았습니다!"
-                  lang="python3"
-                  codeLength="641B"
-                  submitTime="3분 전"
-                />
-                <SubmitList
-                  number="2"
-                  result="맞았습니다!"
-                  lang="python3"
-                  codeLength="681B"
-                  submitTime="19분 전"
-                />
+                {submissionList.map((sb) => {
+                  const timeDiff =
+                    currentDate.getTime() -
+                    new Date(sb.submissionTime).getTime()
+
+                  const seconds = Math.floor(timeDiff / 1000)
+                  const minutes = Math.floor(seconds / 60)
+                  const hours = Math.floor(minutes / 60)
+                  const days = Math.floor(hours / 24)
+                  const months = Math.floor(days / 30)
+
+                  // 제출 시간 설정
+                  let submissionTime
+                  if (days > 30) {
+                    submissionTime = `${months}달 전`
+                  } else if (days > 0) {
+                    submissionTime = `${days}일 전`
+                  } else if (hours > 0) {
+                    submissionTime = `${hours}시간 전`
+                  } else if (minutes > 0) {
+                    submissionTime = `${minutes}분 전`
+                  } else {
+                    submissionTime = '방금 전'
+                  }
+
+                  // result 색상 설정
+                  resultColor =
+                    sb.result === '맞았습니다' ? correctColor : incorrectColor
+
+                  return (
+                    <SubmitList
+                      key={sb.submissionId}
+                      number={sb.submissionId}
+                      result={sb.result}
+                      lang={sb.language}
+                      codeLength={sb.length}
+                      submitTime={submissionTime}
+                      resultColor={resultColor}
+                      code={sb.code}
+                    />
+                  )
+                })}
               </div>
             </div>
           )}
