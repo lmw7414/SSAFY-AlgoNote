de696ec7c7a0c0dda31849d93b9e7c592710f7ac
이민우 <lmw7414@naver.com>
2024-05-10 16:23:25 +0900

    #83 - feat: [BE] 코드 실행 - 파이썬 언어 추가 지원
    실행시간, 메모리 사용량을 측정하고, 파이썬 관련 필요한 설정을 docker를 통해 추가하였다.

--- grading-server/src/main/java/com/ssafy/grading/service/PythonExecutorService.java
+++ grading-server/src/main/java/com/ssafy/grading/service/PythonExecutorService.java
@@ -9,6 +9,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.UUID;
 import java.util.concurrent.*;
@@ -19,7 +20,7 @@ import static com.ssafy.grading.util.CodeInputVerification.normalizeNewlines;
 @Service
 public class PythonExecutorService {
 
-    private static final String PYTHON_RUNNER = "python";
+    private static final String PYTHON_RUNNER = "python3";
 
     public ExecutionResult compileAndExecute(String code, String input, String expected) {
         String dirPath = UUID.randomUUID() + "/";
@@ -30,14 +31,18 @@ public class PythonExecutorService {
             if (!directory.exists())
                 directory.mkdir();
             // 파이썬 코드를 파일로 저장
+            code = addMeasureCode(code);
+            System.out.println(code);
             Files.write(Paths.get(pythonFile), code.getBytes());
             Files.write(Paths.get(inputFileName), input.getBytes());
 
             // run 파이썬
             String output = run(pythonFile, inputFileName);
-            String result = normalizeNewlines(output);
+            String result = normalizeNewlines(getActualOutput(output));
+            double executionTime = getUsedTime(output);
+            double memoryUsage = getUsedMemory(output);
             boolean isCorrect = result.trim().equals(normalizeNewlines(expected.trim()));
-            return new ExecutionResult(result, null, null, isCorrect);
+            return new ExecutionResult(result, executionTime, memoryUsage, isCorrect);
         } catch (Exception e) {
             return new ExecutionResult(e.getMessage(), null, null, null);
         } finally {
@@ -67,8 +72,6 @@ public class PythonExecutorService {
                 return actualOutput;
             });
             return future.get(12, TimeUnit.SECONDS);
-//            String output = new BufferedReader(new InputStreamReader(runProcess.getInputStream()))
-//                    .lines().collect(Collectors.joining("\n"));
 
         } catch (TimeoutException e) {
             future.cancel(true);  // 작업을 취소하고 프로세스를 종료하기
@@ -80,6 +83,44 @@ public class PythonExecutorService {
         } finally {
             executor.shutdownNow();  // 자원 회수
         }
+    }
+
+    private String getActualOutput(String output) {
+        String[] outputToArray = output.split("\n");
+        String[] result = Arrays.copyOfRange(outputToArray, 0, outputToArray.length - 2);
+        return String.join("\n", result);
+    }
+
+    private double getUsedTime(String output) {
+        String[] outputToArray = output.split("\n");
+        return Double.parseDouble(outputToArray[outputToArray.length - 2]);
+    }
+
+    private double getUsedMemory(String output) {
+        String[] outputToArray = output.split("\n");
+        return Double.parseDouble(outputToArray[outputToArray.length - 1]);
+    }
+
+    private String addMeasureCode(String code) {
+        // 시작 시간과 메모리 측정 코드
+        String startCode = "import time\n" +
+                "from memory_profiler import memory_usage\n" +
+                "start_time = time.time()\n" +
+                "start_memory = memory_usage(max_usage=True)\n\n";
+
+        // 종료 시간과 메모리 측정 코드, 그리고 출력
+        String endCode = "\n\nend_time = time.time()\n" +
+                "end_memory = memory_usage(max_usage=True)\n" +
+                "execution_time = (end_time - start_time) * 1000\n" +
+                "used_memory = (end_memory - start_memory) * 1024\n" +
+                "print(execution_time)\n" +
+                "print(used_memory)\n";
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(startCode);
+        builder.append(code);
+        builder.append(endCode);
+        return builder.toString();
     }
 
     // 파일 제거
