468b11975f7fac841ad6b113b728e6058b91547a
김희수 <wlskaka4@gmail.com>
2024-05-10 12:54:53 +0900

    #91 - feat : [BE] 타임존 설정

--- backend/src/main/java/com/ssafy/algonote/AlgonoteApplication.java
+++ backend/src/main/java/com/ssafy/algonote/AlgonoteApplication.java
@@ -1,5 +1,7 @@
 package com.ssafy.algonote;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +11,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 public class AlgonoteApplication {
 
+	@PostConstruct
+	void started(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AlgonoteApplication.class, args);
 	}
