083a1e8889a2cfb65743eca6134ed12bc0993f2a
김희수 <wlskaka4@gmail.com>
2024-05-10 15:26:26 +0900

    #36 - feat : [BE] 추천시스템 fastAPI

--- recommender/main.py
+++ recommender/main.py
@@ -1,69 +1,64 @@
+import uvicorn
+from fastapi import FastAPI, UploadFile, Request, status, File, HTTPException
+from fastapi.responses import JSONResponse, FileResponse
 import os
+import uuid
+from pathlib import Path
 import sys
-import itertools
+import platform
+import json
+from starlette.middleware.cors import CORSMiddleware
+
+from dto.request.RecommendReqDto import RecommendReqDto
+from dto.response.RecommendResDto import RecommendResDto
+
+
+from util.utils import get_user_dict
+from model.model import inference
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-import lightfm
-from lightfm import LightFM
-from lightfm.data import Dataset
-from lightfm import cross_validation
-from lightfm.evaluation import precision_at_k as lightfm_prec_at_k
-from lightfm.evaluation import recall_at_k as lightfm_recall_at_k
 
-from recommenders.evaluation.python_evaluation import precision_at_k, recall_at_k
-from recommenders.utils.timer import Timer
-from recommenders.datasets import movielens
-from recommenders.models.lightfm.lightfm_utils import (
-    track_model_metrics,
-    prepare_test_df,
-    prepare_all_predictions,
-    compare_metric,
-    similar_users,
-    similar_items,
+app = FastAPI()
+
+origins = [
+    "https://k10b203.p.ssafy.io",
+    "http://localhost:8000",
+    "http://0.0.0.0:8000",
+    "http://0.0.0.0",
+]
+user_dict = get_user_dict()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-from recommenders.utils.notebook_utils import store_metadata
 
-# default number of recommendations
-K = 10
-# percentage of data used for testing
-TEST_PERCENTAGE = 0.25
-# model learning rate
-LEARNING_RATE = 0.25
-# no of latent factors
-NO_COMPONENTS = 20
-# no of epochs to fit model
-NO_EPOCHS = 20
-# no of threads to fit model
-NO_THREADS = 32
-# regularisation for both user and item features
-ITEM_ALPHA = 1e-6
-USER_ALPHA = 1e-6
+@app.post("/python/recommend")
+async def recommend(recommendDto : RecommendReqDto):
 
-# seed for pseudonumber generations
-SEED = 42
+    # print(user_dict)
+    try:
+        nickname = recommendDto.nickname
+        tag = recommendDto.tag
+        solvedProblemIds = recommendDto.solvedProblemIds
 
-from get_data import *
+        validation = []
+        for problemId in solvedProblemIds:
+            validation.append([0, problemId, 5])
+        validation = pd.DataFrame(validation, columns=["userID", "itemID", "rating"]) 
 
-arithmetic_data = get_df("arithmetic")
-arithmetic_data.sample(5, random_state=SEED)
+        recommended_problems = inference(validation, tag)
+        recommendResDto = RecommendResDto(count=len(recommended_problems), 
+                                          recommendedProblemIds=recommended_problems)
 
-dataset = Dataset()
-
-dataset.fit(users=arithmetic_data["memberId"], items=arithmetic_data["problemId"])
-
-interactions, weights = dataset.build_interactions(arithmetic_data.iloc[:, 0:3].values)
+        return  recommendResDto
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
-model1 = LightFM(loss='warp', no_components=NO_COMPONENTS, 
-                 learning_rate=LEARNING_RATE,                 
-                 random_state=np.random.RandomState(SEED))
 
-train_interactions, test_interactions = cross_validation.random_train_test_split(
-    interactions, test_percentage=TEST_PERCENTAGE,
-    random_state=np.random.RandomState(SEED))
-
-model1.fit(interactions=train_interactions,
-          epochs=NO_EPOCHS)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ recommender/model/model.py
@@ -0,0 +1,17 @@
+import pickle
+import os
+from recommenders.models.cornac.cornac_utils import predict_ranking
+def inference(validation, tag):
+    path = os.path.join(os.getcwd(), 'model', 'models', f"{tag}_bpr.pkl")
+    with open(path, "rb") as f:
+        model = pickle.load(f)
+    
+    solved = validation["itemID"].unique()
+    
+
+    prediction = predict_ranking(model, validation, usercol='userID', itemcol='itemID', remove_seen=True)
+    prediction = prediction.sort_values("prediction", ascending=False).drop_duplicates(subset="itemID")
+    threshold = prediction["prediction"].quantile(0.95)
+    top_k = prediction[prediction["prediction"] > threshold]
+    top_k = top_k[~top_k["itemID"].isin(solved)]
+    return top_k["itemID"]
