0011e8cb87ae1a1a7a3af004a85b5cb5eca8d0c8
김성재 <sngjae972@gmail.com>
2024-05-13 01:34:28 +0900

    docs : [BE] NotificationService에 대한 JavaDoc 내용 수정

--- backend/src/main/java/com/ssafy/algonote/notification/service/NotificationService.java
+++ backend/src/main/java/com/ssafy/algonote/notification/service/NotificationService.java
@@ -23,22 +23,20 @@ public class NotificationService {
     /**
      * SseEmitter를 구독한다.
      *
-     * @param memberId
-     * @param lastEventId
-     * @return
+     * @param memberId 알림을 수신할 회원의 memberId
+     * @param lastEventId 마지막으로 수신한 알림의 eventId
+     * @return 요청을 보낸 회원에 대한 SseEmitter 객체
      */
     public SseEmitter subscribe(Long memberId, String lastEventId) {
-        String emitterId = makeTimeIncludedId(memberId);  // 데이터가 유실된 시점을 파악할 수 있으므로 저장된 key값 비교를 통해 유실된 데이터만 재전송한다
+        String emitterId = makeTimeIncludedId(memberId);
         SseEmitter emitter = emitterRepository.save(emitterId, new SseEmitter(DEFAULT_TIMEOUT));
 
         emitter.onCompletion(() -> emitterRepository.deleteById(emitterId));
         emitter.onTimeout(() -> emitterRepository.deleteById(emitterId));
 
-        // SSE 연결이 이뤄진 후, 하나의 데이터도 전송되지 않으면 SseEmitter의 유효 시간이 끝난 후 503 Error가 발생하므로 더미 이벤트 전송
         String eventId = makeTimeIncludedId(memberId);
         sendEvent(emitter, eventId, emitterId, "EventStream Created. [memberId=" + memberId + "]");
 
-        // 클라이언트가 미수신한 Event 목록이 존재할 경우 전송하여 Event 유실을 예방
         if (hasLostEvent(lastEventId)) {
             sendLostEvents(memberId, lastEventId, emitterId, emitter);
         }
@@ -49,8 +47,8 @@ public class NotificationService {
     /**
      * memberId에 현재 시각을 붙여 id를 생성한다.
      *
-     * @param memberId
-     * @return
+     * @param memberId 회원 id
+     * @return 현재 시각을 포함한 id
      */
     private static String makeTimeIncludedId(Long memberId) {
         return memberId + "_" + System.currentTimeMillis();
@@ -59,8 +57,8 @@ public class NotificationService {
     /**
      * 클라이언트가 미수신한 event가 존재하는지에 대한 여부 반환한다.
      *
-     * @param lastEventId
-     * @return
+     * @param lastEventId 마지막으로 수신한 이벤트의 id
+     * @return 미수신 event의 존재 여부
      */
     private static boolean hasLostEvent(String lastEventId) {
         return !lastEventId.isEmpty();
@@ -69,9 +67,9 @@ public class NotificationService {
     /**
      * lastEventId를 기준으로 그 이후의 이벤트들을 전송한다.
      *
-     * @param memberId - 알림을 받을 회원의 memberId
-     * @param lastEventId - 마지막으로 받은 알림의 eventId
-     * @param emitter
+     * @param memberId 알림을 받을 회원의 memberId
+     * @param lastEventId 마지막으로 받은 알림의 eventId
+     * @param emitter 이벤트를 전송할 emitter
      */
     private void sendLostEvents(Long memberId, String lastEventId, String emitterId, SseEmitter emitter) {
         Map<String, Object> events = emitterRepository.findAllEventCacheStartWithId(String.valueOf(memberId));
@@ -83,8 +81,8 @@ public class NotificationService {
     /**
      * SseEmitter로 event 전송
      *
-     * @param emitterId   - 데이터를 받을 사용자의 아이디
-     * @param data - 전송할 데이터
+     * @param emitterId 데이터를 받을 사용자의 아이디
+     * @param data 전송할 데이터
      */
     private void sendEvent(SseEmitter emitter, String eventId, String emitterId, Object data) {
         if (emitter != null) {
@@ -104,8 +102,8 @@ public class NotificationService {
     /**
      * 외부 서비스 클래스에서 쓰일 알림 전송 메서드
      *
-     * @param receiver - 알림을 받을 회원
-     * @param content - 알림 내용
+     * @param receiver 알림을 받을 회원
+     * @param content 알림 내용
      */
     public void notify(Member receiver, String content) {
         Notification notification = notificationRepository.save(Notification.of(receiver, content));
