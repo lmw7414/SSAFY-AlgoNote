3c09c83acc0563541ee638111d23700af4e2dc3a
이민우 <lmw7414@naver.com>
2024-05-14 10:49:48 +0900

    grading-server  프로젝트 업로드
    CI/CD 구축을 위해 새로 브랜치를 파서 업로드 함

---
+++ grading-server/src/main/java/com/ssafy/grading/controller/ExecutionController.java
@@ -0,0 +1,45 @@
+package com.ssafy.grading.controller;
+
+
+import com.ssafy.grading.dto.ExecutionResult;
+import com.ssafy.grading.dto.request.Request;
+import com.ssafy.grading.service.CExecutorService;
+import com.ssafy.grading.service.CppExecutorService;
+import com.ssafy.grading.service.JavaExecutorService;
+import com.ssafy.grading.service.PythonExecutorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/execute")
+@RequiredArgsConstructor
+public class ExecutionController {
+    private final JavaExecutorService javaExecutorService;
+    private final PythonExecutorService pythonExecutorService;
+    private final CExecutorService cExecutorService;
+    private final CppExecutorService cppExecutorService;
+    @PostMapping("/java")
+    public ResponseEntity<ExecutionResult> executeJava(@RequestBody Request request) {
+        return ResponseEntity.ok(javaExecutorService.compileAndExecute(request.sourceCode(), request.inputData(), request.expectedOutput()));
+    }
+
+    @PostMapping("/py")
+    public ResponseEntity<ExecutionResult> executePython(@RequestBody Request request) {
+        return ResponseEntity.ok(pythonExecutorService.compileAndExecute(request.sourceCode(), request.inputData(), request.expectedOutput()));
+    }
+
+    @PostMapping("/c")
+    public ResponseEntity<ExecutionResult> executeC(@RequestBody Request request) {
+        return ResponseEntity.ok(cExecutorService.compileAndExecute(request.sourceCode(), request.inputData(), request.expectedOutput()));
+    }
+
+    @PostMapping("/cpp")
+    public ResponseEntity<ExecutionResult> executeCpp(@RequestBody Request request) {
+        return ResponseEntity.ok(cppExecutorService.compileAndExecute(request.sourceCode(), request.inputData(), request.expectedOutput()));
+    }
+
+}
