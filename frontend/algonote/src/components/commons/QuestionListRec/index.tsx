d903d3a6d51356e0342f5d93a95445f3b07372b4
LEK <eung9702@gmail.com>
2024-05-15 02:43:10 +0900

    feat: [FE] 문제 추천 문제 목록 컴포넌트 개발(중)

---
+++ frontend/algonote/src/components/commons/QuestionListRec/index.tsx
@@ -0,0 +1,89 @@
+import { useRouter } from 'next/router'
+import { SimpleButton } from '../Buttons/Button'
+import TierImg from '../Tier'
+import styles from './QuestionList.module.scss'
+
+interface QuestionListProps {
+  id: number
+  title: string
+  tier: number
+  tags: string[]
+  complete: string
+  time: string
+}
+
+const QuestionListRec = ({
+  id,
+  title,
+  tier,
+  tags,
+  complete,
+  time,
+}: QuestionListProps) => {
+  const router = useRouter()
+  const handleWriteNote = () => {
+    const problemId = id
+    router.push({
+      pathname: `https://www.acmicpc.net/problem/${problemId}`,
+      query: { id: problemId, title },
+    })
+  }
+
+  return (
+    <div>
+      <div className={styles.wrapper}>
+        <div className={styles.level}>
+          <TierImg tier={tier} />
+        </div>
+        <div className={styles.questionNumber}>
+          <p>{id}</p>
+        </div>
+        <div className={styles.title}>
+          <p>{title}</p>
+        </div>
+        <div className={styles.type}>
+          <p>#{tags.join(' #')}</p>
+        </div>
+        <div className={styles.summitDate}>
+          <p>{time}</p>
+        </div>
+        <div className={styles.writeNote}>
+          {complete === 'NOT_YET' ? (
+            <SimpleButton
+              text="노트 작성하기"
+              onClick={handleWriteNote}
+              style={{
+                background: '#ffffff',
+                border: '1.3px solid #3c87fe',
+                color: '#3c87fe',
+                fontSize: '0.9rem',
+                fontFamily: 'Pretendard',
+                width: '6.7rem',
+                height: '2.2rem',
+                borderRadius: '8px',
+                fontWeight: '500',
+              }}
+            />
+          ) : (
+            <SimpleButton
+              text="노트 보러가기"
+              onClick={handleWriteNote}
+              style={{
+                border: 'none',
+                fontSize: '0.9rem',
+                fontFamily: 'Pretendard',
+                width: '6.7rem',
+                height: '2.2rem',
+                borderRadius: '8px',
+                fontWeight: '500',
+              }}
+            />
+          )}
+        </div>
+      </div>
+      <hr className={styles.bottomHr} />
+    </div>
+  )
+}
+
+export default QuestionListRec
