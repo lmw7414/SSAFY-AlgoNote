2e278709e5df1cfb5865af14f31a1b99c518ce33
이은규 <eung9702@gmail.com>
2024-05-15 17:20:31 +0900

    feat: [FE] 추천문제 리스트 컴포넌트 제작

--- frontend/algonote/src/components/commons/QuestionListRec/index.tsx
+++ frontend/algonote/src/components/commons/QuestionListRec/index.tsx
@@ -1,38 +1,29 @@
 import { useRouter } from 'next/router'
-import { SimpleButton } from '../Buttons/Button'
 import TierImg from '../Tier'
-import styles from './QuestionList.module.scss'
+import styles from './QuestionListRec.module.scss'
 
 interface QuestionListProps {
   id: number
   title: string
   tier: number
-  tags: string[]
-  complete: string
-  time: string
+  acceptedUserCount: number
+  averageTries: number
 }
 
 const QuestionListRec = ({
   id,
   title,
   tier,
-  tags,
-  complete,
-  time,
+  acceptedUserCount,
+  averageTries,
 }: QuestionListProps) => {
   const router = useRouter()
-  const handleWriteNote = () => {
-    const problemId = id
-    router.push({
-      pathname: `https://www.acmicpc.net/problem/${problemId}`,
-      query: { id: problemId, title },
-    })
-  }
 
   return (
     <div>
       <div className={styles.wrapper}>
         <div className={styles.level}>
+          <div className={styles.blank} />
           <TierImg tier={tier} />
         </div>
         <div className={styles.questionNumber}>
@@ -41,44 +32,11 @@ const QuestionListRec = ({
         <div className={styles.title}>
           <p>{title}</p>
         </div>
-        <div className={styles.type}>
-          <p>#{tags.join(' #')}</p>
+        <div className={styles.acceptedUserCount}>
+          <p>{acceptedUserCount}</p>
         </div>
-        <div className={styles.summitDate}>
-          <p>{time}</p>
-        </div>
-        <div className={styles.writeNote}>
-          {complete === 'NOT_YET' ? (
-            <SimpleButton
-              text="노트 작성하기"
-              onClick={handleWriteNote}
-              style={{
-                background: '#ffffff',
-                border: '1.3px solid #3c87fe',
-                color: '#3c87fe',
-                fontSize: '0.9rem',
-                fontFamily: 'Pretendard',
-                width: '6.7rem',
-                height: '2.2rem',
-                borderRadius: '8px',
-                fontWeight: '500',
-              }}
-            />
-          ) : (
-            <SimpleButton
-              text="노트 보러가기"
-              onClick={handleWriteNote}
-              style={{
-                border: 'none',
-                fontSize: '0.9rem',
-                fontFamily: 'Pretendard',
-                width: '6.7rem',
-                height: '2.2rem',
-                borderRadius: '8px',
-                fontWeight: '500',
-              }}
-            />
-          )}
+        <div className={styles.averageTries}>
+          <p>{averageTries}</p>
         </div>
       </div>
       <hr className={styles.bottomHr} />
