b6463f198c1cb0350af1d9fa971a00975def98c5
이민우 <lmw7414@naver.com>
2024-05-16 02:13:19 +0900

    #58 - feat : [FE] 전체 검색 페이지

--- frontend/algonote/src/pages/search/index.tsx
+++ frontend/algonote/src/pages/search/index.tsx
@@ -1,7 +1,60 @@
-import SearchInput from '@/components/commons/SearchInput'
+'use client'
 
-const Search = () => {
-  return <SearchInput />
+import { useEffect } from 'react'
+import styles from '@/pages/bookmark/bookmark.module.scss'
+import Notes from '@/components/commons/Bookmark/Note'
+import ResultNote from '@/components/commons/Bookmark/ResultNote'
+import { FilterButton } from '@/components/commons/Buttons/Button'
+import SearchInput from '@/components/commons/SearchInput'
+import useSearchResult from '@/stores/search-store'
+
+interface FilterSectionProps {
+  title: string
+  items: string[]
+  itemKey: string
 }
 
-export default Search
+const tier = ['브론즈', '실버', '골드', '플레티넘', '다이아', '루비'] //
+const category = ['구현', '그리디', '그래프', '플래티넘', 'DP', '자료구조'] // note.title
+
+const FilterSection = ({ title, items, itemKey }: FilterSectionProps) => {
+  return (
+    <div className={styles.bookmark}>
+      <div>{title}</div>
+      <div className={styles.filterButton}>
+        {items.map((it, index) => {
+          const tierKey = `${itemKey}-${index}`
+          return (
+            <div key={tierKey}>
+              <FilterButton
+                text={it}
+                onClick={() => console.log('눌렸음')}
+                className="search"
+              />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+const Bookmark = () => {
+  const { isSearched, resetSearch } = useSearchResult()
+
+  useEffect(() => {
+    resetSearch()
+  }, [])
+
+  return (
+    <div className={styles.frame}>
+      <SearchInput />
+      <FilterSection title="티어" items={tier} itemKey="tier" />
+      <FilterSection title="유형" items={category} itemKey="category" />
+      <div className={styles.division_line} />
+      {isSearched ? <ResultNote /> : <Notes />}
+    </div>
+  )
+}
+
+export default Bookmark
