074622db90d080d55e4a0e865ccda8c333020ce8
조은영 <hdtv0218@gmail.com>
2024-05-17 00:59:56 +0900

    #58 - feat : [FE] 검색결과가 없는 경우 예외처리

--- frontend/algonote/src/components/commons/Bookmark/ResultNote.tsx
+++ frontend/algonote/src/components/commons/Bookmark/ResultNote.tsx
@@ -12,52 +12,57 @@ const ResultNote = () => {
   const { searchResult } = useSearchResult()
   const router = useRouter()
 
+  console.log('검색 결과', searchResult.noteCnt)
+
   const filteredNotes =
     router.asPath === '/bookmark'
       ? searchResult.notes.filter((it) => it.bookmarked)
       : searchResult.notes
 
-  console.log('필터링 노트', filteredNotes)
   return (
     <div className={styles.frame}>
-      {filteredNotes.map((it) => {
-        return (
-          <div
-            key={it.noteId}
-            className={styles.note}
-            onClick={() => handleDetailNote(it.noteId, router)}
-            onKeyDown={(e) => handleKeyPress(e, it.noteId, router)}
-            role="button"
-            tabIndex={0}
-          >
-            <div className={styles.content}>
-              <div className={styles.problem}>
-                <div className={styles.tierImage}>
-                  <TierImg tier={it.problemTier} />
+      {searchResult.noteCnt === 0 ? (
+        <div className={styles.noResults}>아직 저장된 노트가 없어요!</div>
+      ) : (
+        filteredNotes.map((it) => {
+          return (
+            <div
+              key={it.noteId}
+              className={styles.note}
+              onClick={() => handleDetailNote(it.noteId, router)}
+              onKeyDown={(e) => handleKeyPress(e, it.noteId, router)}
+              role="button"
+              tabIndex={0}
+            >
+              <div className={styles.content}>
+                <div className={styles.problem}>
+                  <div className={styles.tierImage}>
+                    <TierImg tier={it.problemTier} />
+                  </div>
+                  <div className={styles.problemTitle}>{it.problemTitle}</div>
                 </div>
-                <div className={styles.problemTitle}>{it.problemTitle}</div>
-              </div>
-              <div className={styles.note_title}>{it.noteTitle}</div>
-              <div className={styles.details}>
-                <div className={styles.countNickname}>
-                  {router.asPath === '/search' && (
-                    <div className={styles.heart}>
-                      <Image
-                        src={redHeart}
-                        alt="좋아요 개수"
-                        width={25}
-                        height={22}
-                      />
-                      {it.heartCnt}
-                    </div>
-                  )}
-                  <div className={styles.nickname}>{it.memberNickname}</div>
+                <div className={styles.note_title}>{it.noteTitle}</div>
+                <div className={styles.details}>
+                  <div className={styles.countNickname}>
+                    {router.asPath === '/search' && (
+                      <div className={styles.heart}>
+                        <Image
+                          src={redHeart}
+                          alt="좋아요 개수"
+                          width={25}
+                          height={22}
+                        />
+                        {it.heartCnt}
+                      </div>
+                    )}
+                    <div className={styles.nickname}>{it.memberNickname}</div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        )
-      })}
+          )
+        })
+      )}
     </div>
   )
 }
