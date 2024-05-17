793593ad950ae7a1d079c8b277faf48b8b53f414
조은영 <hdtv0218@gmail.com>
2024-05-17 15:03:47 +0900

    #143 - feat : [FE] 코드 비교 로직 수정 - 코드 하나만 넣어도 동작

--- frontend/algonote/src/components/commons/CodeSelectButton/ExcuteResult.tsx
+++ frontend/algonote/src/components/commons/CodeSelectButton/ExcuteResult.tsx
@@ -53,7 +53,7 @@ const ExecuteResult = ({
         isDisabled: true,
         alertText: '상단에 입출력 데이터를 넣어주세요',
       })
-    } else if (codes[0].length === 1 || codes[1].length === 1) {
+    } else if (codes[0].length === 1 && codes[1].length === 1) {
       setInputValue({
         isDisabled: true,
         alertText: '비교할 코드를 넣어주세요',
@@ -100,37 +100,50 @@ const ExecuteResult = ({
         </div>
       </div>
       <div className={style.resultBox}>
-        {executeResult.map((result, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <div key={index} className={style.result}>
-            <div>
-              <p className={style.title}>실행 결과</p>
-              <p
-                style={{
-                  color: result.isCorrect ? correctColor : incorrectColor,
-                  fontWeight: 600,
-                  marginBottom: '1rem',
-                }}
-              >
-                {JSON.stringify(result.isCorrect) ? '맞았습니다' : '틀렸습니다'}
-              </p>
-              <p>
-                <strong>출력 | </strong> {JSON.stringify(result.output)}
-              </p>
-              <p>
-                <strong>실행 시간 | </strong>
-                {JSON.stringify(result.executionTime)}
-              </p>
-              <p>
-                <strong>메모리 | </strong> {JSON.stringify(result.memoryUsage)}
-              </p>
-            </div>
-            <div>
-              <p>{JSON.stringify(complexityResult[index].timeComplexity)}</p>
-              <p>{JSON.stringify(complexityResult[index].spaceComplexity)}</p>
-            </div>
-          </div>
-        ))}
+        {executeResult.map(
+          (result, index) =>
+            codes[index] !== ' ' && ( // eslint-disable-next-line react/no-array-index-key
+              <div key={index} className={style.result}>
+                <div>
+                  <p className={style.title}>실행 결과</p>
+                  <p
+                    style={{
+                      color: result.isCorrect ? correctColor : incorrectColor,
+                      fontWeight: 600,
+                      marginBottom: '1rem',
+                    }}
+                  >
+                    {JSON.stringify(result.isCorrect)
+                      ? '맞았습니다'
+                      : '틀렸습니다'}
+                  </p>
+                  <p>
+                    <strong>출력 | </strong> {JSON.stringify(result.output)}
+                  </p>
+                  <p>
+                    <strong>실행 시간 | </strong>
+                    {result.executionTime !== null
+                      ? JSON.stringify(result.executionTime)
+                      : '계산 불가'}
+                  </p>
+                  <p>
+                    <strong>메모리 | </strong>
+                    {result.memoryUsage !== null
+                      ? JSON.stringify(result.memoryUsage)
+                      : '계산 불가'}
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    {JSON.stringify(complexityResult[index].timeComplexity)}
+                  </p>
+                  <p>
+                    {JSON.stringify(complexityResult[index].spaceComplexity)}
+                  </p>
+                </div>
+              </div>
+            ),
+        )}
       </div>
     </div>
   )
