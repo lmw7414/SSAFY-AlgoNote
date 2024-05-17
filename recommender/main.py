0ccf194d8cdd2c700162c51e73dfeb78deba7ca0
김희수 <wlskaka4@gmail.com>
2024-05-17 09:21:13 +0900

    main.py에서 호스트 변경

--- recommender/main.py
+++ recommender/main.py
@@ -72,5 +72,5 @@ async def recommend(tag: str = Query(...), page: int=Query(0), memberId: int=Que
 
 
 if __name__ == "__main__":
-    # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    # uvicorn.run("main:app", host="localhost", port=8000, reload=True)
