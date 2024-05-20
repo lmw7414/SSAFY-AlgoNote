6d79b06ee0b512521e04d83ced73d81ccbe7d005
조은영 <hdtv0218@gmail.com>
2024-05-20 23:04:21 +0900

    feat : [FE] 메타 태그 추가

--- frontend/algonote/src/pages/_document.tsx
+++ frontend/algonote/src/pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="균형잡힌 알고리즘 공부 알고노트로 시작하세요"
+        />
+        <meta property="og:title" content="algonote" />
+        <meta
+          property="og:description"
+          content="균형잡힌 알고리즘 공부 알고노트로 시작하세요"
+        />
+        <meta property="og:image" content="images/longLogo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <div id="modal-root" />
